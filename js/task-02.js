417c0738ecce1d30b6258922970b4741f825f83a
Viktor-117 <viktor.hushchin@gmail.com>
2022-07-25 12:44:23 +0300

    3 tasks complete

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const itemEl = ingredients.map((ingredient) => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = ingredient;
+  return liEl;
+});
+
+listEl.append(...itemEl);
